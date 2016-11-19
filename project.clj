dced4081a78ed8e0aaa2c61c1d5fe7e58d7cb8b0
Jörg Richter <jri@deepamehta.de>
2016-11-19 18:33:27 +0100

    Implement DM4 TopicModel interface in Clojure

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [de.deepamehta/deepamehta-core "4.8.4"]
                  [com.datomic/datomic-pro "0.9.5407"]]
-  :main dm5.core)
+  :profiles {:uberjar {:aot :all}}
+  :repl-options {:init-ns dm5.core}
+)
--- src/dm5/core.clj
+++ src/dm5/core.clj
@@ -2,6 +2,17 @@
     (:require [datomic.api :as d]))
 
 
+;; === Core API ===
+
+(defrecord TopicModelImpl [id uri type-uri value childs]
+  de.deepamehta.core.model.TopicModel
+    (getId [_] id)
+    (getUri [_] uri)
+    (getTypeUri [_] type-uri)
+    (getSimpleValue [_] value)
+    (getChildTopicsModel [_] childs))
+
+
 ;; === Setup DB ===
 
 (def db-uri "datomic:mem://deepamehta")
@@ -15,14 +26,19 @@
 
 ;; === Storage API ===
 
-(defn get-topic [id] (d/touch (d/entity (d/db conn) id)))
+(defn get-topic [id] (let [entity (d/entity (d/db conn) id)] (TopicModelImpl.
+  (:db/id entity)
+  (:dm5.object/uri entity)
+  (:dm5.object/type-uri entity)
+  (:dm5.object/string-value entity)
+  nil)))
 
 (defn all-topics [] (d/q '[:find (pull ?p [*]) :where [?p :dm5.object/type-uri]] (d/db conn)))
 
 (defn create-topic [uri type-uri value]
-    @(d/transact conn [{
-    :db/id #db/id[:db.part/user -1]
-    :dm5.object/uri uri
-    :dm5.object/type-uri type-uri
-    :dm5.object/string-value value
-    }]))
+  @(d/transact conn [{
+  :db/id #db/id[:db.part/user -1]
+  :dm5.object/uri uri
+  :dm5.object/type-uri type-uri
+  :dm5.object/string-value value
+  }]))
