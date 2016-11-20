0c7aae0cffe9f2e80d128fb6f36aafbc22d1a8f2
Jörg Richter <jri@deepamehta.de>
2016-11-20 16:12:47 +0100

    Add get-related-topics function

--- src/dm5/core.clj
+++ src/dm5/core.clj
@@ -31,24 +31,30 @@
   (:dm5.object/string-value e)
   nil)))
 
+(defn get-related-topics [id] (d/q '[:find ?t :in $ ?id :where [?r1 :dm5.role/player ?id]
+                                                               [?a :dm5.assoc/role ?r1]
+                                                               [?a :dm5.assoc/role ?r2]
+                                                               [(!= ?r1 ?r2)]
+                                                               [?r2 :dm5.role/player ?t]] (d/db conn) id))
+
 (defn all-topics [] (d/q '[:find (pull ?p [*]) :where [?p :dm5/entity-type :dm5.entity-type/topic]] (d/db conn)))
 (defn all-assocs [] (d/q '[:find (pull ?p [*]) :where [?p :dm5/entity-type :dm5.entity-type/assoc]] (d/db conn)))
 
 (defn create-topic [uri type value]
   (let [tempid (d/tempid :db.part/user)
         tx-info @(d/transact conn [{:db/id tempid
-                               :dm5/entity-type :dm5.entity-type/topic
-                               :dm5.object/uri uri
-                               :dm5.object/type type
-                               :dm5.object/string-value value}])]
+                                    :dm5/entity-type :dm5.entity-type/topic
+                                    :dm5.object/uri uri
+                                    :dm5.object/type type
+                                    :dm5.object/string-value value}])]
        (d/resolve-tempid (d/db conn) (:tempids tx-info) tempid)))
 
 (defn create-assoc [uri type value roles]
   (let [tempid (d/tempid :db.part/user)
         tx-info @(d/transact conn [{:db/id tempid
-                               :dm5/entity-type :dm5.entity-type/assoc
-                               :dm5.object/uri uri
-                               :dm5.object/type type
-                               :dm5.object/string-value value
-                               :dm5.assoc/role roles}])]
+                                    :dm5/entity-type :dm5.entity-type/assoc
+                                    :dm5.object/uri uri
+                                    :dm5.object/type type
+                                    :dm5.object/string-value value
+                                    :dm5.assoc/role roles}])]
        (d/resolve-tempid (d/db conn) (:tempids tx-info ) tempid)))
