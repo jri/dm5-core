f5c0aedadff170e978b9979bb510bbe2cebe9559
Jörg Richter <jri@deepamehta.de>
2016-11-22 03:50:27 +0100

    Traversal: add association type filter

--- src/dm5/core.clj
+++ src/dm5/core.clj
@@ -15,21 +15,51 @@
 ;; === DB Access ===
 
 (def rule-set '[
-  ;; assoc type filter: ?t is type of a?
-  [(assoc-type ?a ?t)
-   [?a :dm5.object/type ?t]]
-  ;; 1-hop traversal: ?rel-t is a related topic of ?t
-  [(related-topic ?t ?rel-t)
+  ;; 1-hop traversal: ?t is related to ?rel-t via ?a
+  [(related-topic ?t ?rel-t ?a)
    [?r1 :dm5.role/player ?t]
    [?a :dm5.assoc/role ?r1]
    [?a :dm5.assoc/role ?r2]
    [(!= ?r1 ?r2)]
    [?r2 :dm5.role/player ?rel-t]]
+  ;; type filter: object ?o (topic or assoc) is of type ?t
+  [(object-type ?o ?t)
+   [?o :dm5.object/type ?t]]
+  ;; role type filter: role ?r is of type ?t
+  [(role-type ?r ?t)
+   [?r :dm5.role/type ?t]]
 ])
 
 (defn fetch-topic [id] (d/touch (d/entity (d/db conn) id)))
 
-(defn fetch-related-topics [id] (d/q '[:find ?t :in $ % ?id :where (related-topic ?id ?t)] (d/db conn) rule-set id))
+;; traversal
+
+(declare traversal-query)
+(declare assoc-type-filter)
+
+(defn fetch-related-topics
+  "1-hop traversal
+  opts is a map with 4 optional entries :assoc-type :my-role-type :others-role-type :others-topic-type"
+  ([id] (fetch-related-topics id nil))
+  ([id opts] (let [query (traversal-query id opts)]
+                  (apply d/q {:find '[?t ?a]
+                              :in (query :in)
+                              :where (query :where)} (d/db conn) rule-set (query :args)))))
+
+(defn traversal-query
+  "Returns a traveral query, a map with 4 entries :in :where :args :opts"
+  [id opts]
+  (let [query {:in '[$ % ?id] :where '[(related-topic ?id ?t ?a)] :args [id]}]
+       (-> query (assoc-type-filter opts))))
+
+(defn assoc-type-filter [query opts]
+  (let [at (:assoc-type opts)]
+       (if at {:in    (conj (:in    query) '?at)
+               :where (conj (:where query) '(object-type ?a ?at))
+               :args  (conj (:args  query) at)}
+              query)))
+
+;;
 
 (defn all-topics [] (d/q '[:find (pull ?p [*]) :where [?p :dm5/entity-type :dm5.entity-type/topic]] (d/db conn)))
 (defn all-assocs [] (d/q '[:find (pull ?p [*]) :where [?p :dm5/entity-type :dm5.entity-type/assoc]] (d/db conn)))
