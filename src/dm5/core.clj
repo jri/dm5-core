82fdfebcc968e130fdafb882cca59fee9a2999b9
Jörg Richter <jri@deepamehta.de>
2016-11-23 05:45:49 +0100

    Add by-type and by-value queries

--- src/dm5/core.clj
+++ src/dm5/core.clj
@@ -1,2 +1,41 @@
 (ns dm5.core
-    (:require [dm5.datomic :as db]))
+  (:require [dm5.datomic :as db]))
+
+
+
+;; mockup some type defs
+(def types {
+  ;; Person
+  :dm5.contacts.person {
+    :data-type :composite
+    :assoc-defs {
+      :dm5.contacts.person_name {:cardinality :one}
+      :dm5.contacts.address {:cardinality :one}
+    }
+  }
+  :dm5.contacts.person_name {
+    :data-type :composite
+    :assoc-defs {
+      :dm5.contacts.first_name {:cardinality :one}
+      :dm5.contacts.last_name {:cardinality :one}
+    }
+  }
+  :dm5.contacts.first_name {:data-type :text}
+  :dm5.contacts.last_name {:data-type :text}
+  ;; Address
+  :dm5.contacts.address {
+    :data-type :composite
+    :assoc-defs {
+      :dm5.contacts.street {:cardinality :one}
+      :dm5.contacts.street_number {:cardinality :one}
+      :dm5.contacts.postal_code {:cardinality :one}
+      :dm5.contacts.city {:cardinality :one}
+      :dm5.contacts.country {:cardinality :one}
+    }
+  }
+  :dm5.contacts.street {:data-type :text}
+  :dm5.contacts.street_number {:data-type :text}
+  :dm5.contacts.postal_code {:data-type :text}
+  :dm5.contacts.city {:data-type :text}
+  :dm5.contacts.country {:data-type :text}
+})
--- src/dm5/datomic.clj
+++ src/dm5/datomic.clj
@@ -1,5 +1,6 @@
 (ns dm5.datomic
-    (:require [datomic.api :as d]))
+  (:require [datomic.api :as d])
+  (:use [clojure.pprint :only [pprint]]))
 
 
 ;; === DB Setup ===
@@ -14,8 +15,8 @@
 
 ;; === DB Access ===
 
-(def rule-set '[
-  ;; 1-hop traversal: ?t is related to ?rel-t via ?a
+(def rules '[
+  ;; traversal filter: ?t is related to ?rel-t via ?a
   [(related-topic ?t ?rel-t ?a ?r1 ?r2)
    [?r1 :dm5.role/player ?t]
    [?a :dm5.assoc/role ?r1]
@@ -25,12 +26,18 @@
   ;; type filter: object ?o (topic or assoc) is of type ?t
   [(object-type ?o ?t)
    [?o :dm5.object/type ?t]]
+  ;; value filter: object ?o (topic or assoc) has value ?v
+  [(object-value ?o ?v)
+   [?o :dm5.object/string-value ?v]]
   ;; role type filter: role ?r is of type ?t
   [(role-type ?r ?t)
    [?r :dm5.role/type ?t]]
+  ;; entity type filter: entity ?e is of type ?t
+  [(entity-type ?e ?t)
+   [?e :dm5/entity-type ?t]]
 ])
 
-(defn fetch-topic [id] (d/touch (d/entity (d/db conn) id)))
+(defn fetch-object [id] (d/touch (d/entity (d/db conn) id)))
 
 ;; traversal
 
@@ -41,9 +48,9 @@
   ;; TODO: rather pass map instead of varargs in favor for prgrammatic calling
   [id & {:keys [:assoc-type :my-role-type :others-role-type :others-type] :as opts}]
   (let [builder (query-builder id opts)]
-       (apply d/q (:query builder) (d/db conn) rule-set (:args builder))))
+       (apply d/q (:query builder) (:args builder))))
 
-(defn query-builder
+(defn- query-builder
   "Returns a query builder for a 1-hop traversal that starts at id and is constrained by the opts map.
   The options map can contain these 4 optional keys: :assoc-type :my-role-type :others-role-type :others-type.
   The returned builder has this structure:
@@ -51,13 +58,13 @@
        :args []}"
   [id opts]
   (let [builder {:query {:find '[?t ?a] :in '[$ % ?id] :where '[(related-topic ?id ?t ?a ?r1 ?r2)]}
-                 :args [id]}]
+                 :args [(d/db conn) rules id]}]
        (-> builder (add-filter opts :assoc-type       '?at  '(object-type ?a  ?at))
                    (add-filter opts :my-role-type     '?rt1 '(role-type   ?r1 ?rt1))
                    (add-filter opts :others-role-type '?rt2 '(role-type   ?r2 ?rt2))
                    (add-filter opts :others-type      '?ot  '(object-type ?t  ?ot)))))
 
-(defn add-filter [builder opts opt-key in-sym where-sym]
+(defn- add-filter [builder opts opt-key in-sym where-sym]
   (let [opt-val (opt-key opts)]
        (if opt-val (-> builder (update-in [:query :in]    conj in-sym)
                                (update-in [:query :where] conj where-sym)
@@ -66,8 +73,22 @@
 
 ;;
 
-(defn all-topics [] (d/q '[:find (pull ?p [*]) :where [?p :dm5/entity-type :dm5.entity-type/topic]] (d/db conn)))
-(defn all-assocs [] (d/q '[:find (pull ?p [*]) :where [?p :dm5/entity-type :dm5.entity-type/assoc]] (d/db conn)))
+(defn fetch-by-type [type]
+  (d/q '[:find ?o :in $ % ?t :where (object-type ?o ?t)] (d/db conn) rules type))
+
+(defn fetch-by-value [type value]
+  (d/q '[:find ?o :in $ % ?t ?v :where (object-type ?o ?t)
+                                       (object-value ?o ?v)] (d/db conn) rules type value))
+
+;;
+
+(defn- fetch-all-entities [type]
+  (d/q '[:find (pull ?e [*]) :in $ % ?t :where (entity-type ?e ?t)] (d/db conn) rules type))
+
+(defn fetch-all-topics [] (fetch-all-entities :dm5.entity-type/topic))
+(defn fetch-all-assocs [] (fetch-all-entities :dm5.entity-type/assoc))
+
+;;
 
 (defn store-topic [uri type value]
   (let [tempid (d/tempid :db.part/user)
@@ -98,6 +119,6 @@
 
 (defrecord DatomicStorage []
   DeepaMehtaStorage
-    (fetchTopic [this id]             (fetch-topic id))
+    (fetchTopic [this id]             (fetch-object id))
     (storeTopic [this uri type value] (store-topic uri type value))
 )
