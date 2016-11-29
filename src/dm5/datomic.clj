e7d14849060b3e3d1b8b4c8003f062ec3cdbd718
Jörg Richter <jri@deepamehta.de>
2016-11-29 21:14:45 +0100

    Drop db.install/_attribute calls from schema

--- src/dm5/datomic.clj
+++ src/dm5/datomic.clj
@@ -6,11 +6,14 @@
 ;; === DB Setup ===
 
 (let [db-uri "datomic:mem://deepamehta"]
+  (println "\nCreating database at URI" db-uri)
   (d/create-database db-uri)
   (def conn (d/connect db-uri)))
 
 ;; install schema
-@(d/transact conn (load-file "../dm5-core/resources/dm5-core-schema.edn"))
+(let [schema-file "../dm5-core/resources/dm5-core-schema.edn"]
+  (println "Loading schema file" schema-file "\n")
+  (d/transact conn (load-file schema-file)))
 
 
 ;; === DB Access ===
